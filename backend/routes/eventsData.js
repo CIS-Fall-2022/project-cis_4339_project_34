7222633bb6f071ac303d75d47f5498b4b35f83d6
dooran <70468543+dooran@users.noreply.github.com>
2022-10-07 04:13:18 -0500

    Update eventsData.js

--- backend/routes/eventsData.js
+++ backend/routes/eventsData.js
@@ -1,12 +1,12 @@
-const express = require("express"); 
-const router = express.Router(); 
+const express = require("express");
+const router = express.Router();
 
 //importing data model schemas
-const primaryModel = require("../models/primaryd");
+const eventModel = require("../models/event");
 
 //GET all entries
-router.get("/allpdata/", (req, res, next) => { 
-    primaryModel.find( 
+router.get("/allevents/", (req, res, next) => { 
+    eventModel.find( 
         (error, data) => {
             if (error) {
                 return next(error);
@@ -18,31 +18,28 @@ router.get("/allpdata/", (req, res, next) => {
 });
 
 //GET single entry by ID
-router.get("/pdataid/:id", (req, res, next) => {
-    primaryModel.find( 
-        { _id: req.params.id }, 
-        (error, data) => {
-            if (error) {
-                return next(error);
-            } else {
-                res.json(data);
-            }
+router.get("/eventid/:id", (req, res, next) => { 
+    eventModel.find({ _id: req.params.id }, (error, data) => {
+        if (error) {
+            return next(error)
+        } else {
+            res.json(data)
         }
-    );
+    })
 });
 
 //GET entries based on search query
-//Ex: '...?firstName=Bob&lastName=&searchBy=name' 
+//Ex: '...?eventName=Food&searchBy=name' 
 router.get("/search/", (req, res, next) => { 
     let dbQuery = "";
     if (req.query["searchBy"] === 'name') {
-        dbQuery = { firstName: { $regex: `^${req.query["firstName"]}`, $options: "i" }, lastName: { $regex: `^${req.query["lastName"]}`, $options: "i" } }
-    } else if (req.query["searchBy"] === 'number') {
+        dbQuery = { eventName: { $regex: `^${req.query["eventName"]}`, $options: "i" } }
+    } else if (req.query["searchBy"] === 'date') {
         dbQuery = {
-            "phoneNumbers.primaryPhone": { $regex: `^${req.query["phoneNumbers.primaryPhone"]}`, $options: "i" }
+            date:  req.query["eventDate"]
         }
     };
-    primaryModel.find( 
+    eventModel.find( 
         dbQuery, 
         (error, data) => { 
             if (error) {
@@ -54,32 +51,38 @@ router.get("/search/", (req, res, next) => {
     );
 });
 
-//GET events for a single client
-router.get("/pdata/:id", (req, res, next) => { 
-    
-});
-
-//POST
-router.post("/addpdata", (req, res, next) => { 
-    primaryModel.create( 
-        req.body,
+//GET events for which a client is signed up
+router.get("/client/:id", (req, res, next) => { 
+    eventModel.find( 
+        { attendees: req.params.id }, 
         (error, data) => { 
             if (error) {
                 return next(error);
             } else {
-                res.json(data); 
+                res.json(data);
             }
         }
     );
-    primaryModel.createdAt;
-    primaryModel.updatedAt;
-    primaryModel.createdAt instanceof Date;
 });
 
-//PUT update (make sure req body doesn't have the id)
-router.put("/:id", (req, res, next) => { 
-    primaryModel.findOneAndUpdate( 
-        { _id: req.params.id }, 
+//POST
+router.post("/addevent/", (req, res, next) => { 
+    eventModel.create( 
+        req.body, 
+        (error, data) => { 
+            if (error) {
+                return next(error);
+            } else {
+                res.json(data);
+            }
+        }
+    );
+});
+
+//PUT
+router.put("/:id", (req, res, next) => {
+    eventModel.findOneAndUpdate(
+        { _id: req.params.id },
         req.body,
         (error, data) => {
             if (error) {
@@ -91,17 +94,47 @@ router.put("/:id", (req, res, next) => {
     );
 });
 
+//PUT add attendee to event
+router.put("/addAttendee/:id", (req, res, next) => {
+    //only add attendee if not yet signed uo
+    eventModel.find( 
+        { _id: req.params.id, attendees: req.body.attendee }, 
+        (error, data) => { 
+            if (error) {
+                return next(error);
+            } else {
+                if (data.length == 0) {
+                    eventModel.updateOne(
+                        { _id: req.params.id }, 
+                        { $push: { attendees: req.body.attendee } },
+                        (error, data) => {
+                            if (error) {
+                                consol
+                                return next(error);
+                            } else {
+                                res.json(data);
+                            }
+                        }
+                    );
+                }
+                
+            }
+        }
+    );
+    
+});
+
 //DELETE pdata by _id
 router.delete('/:id', (req, res, next) => {
     //mongoose will use _id of document
-    primaryModel.findOneAndRemove({ _id: req.params.id }, (error, data) => {
+    eventModel.findOneAndRemove({ _id: req.params.id }, (error, data) => {
         if (error) {
             return next(error);
         } else {
             res.status(200).json({
                 msg: data
             });
-            //  res.send('pdata is deleted');
+            //  res.send('event is deleted');
         }
     });
 });
